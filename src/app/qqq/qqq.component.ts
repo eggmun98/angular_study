611d21902be75b2c05bb06b65b7bb8216181871e
eggmun98 <eggmun98@gmail.com>
2023-07-24 17:36:49 +0900

    [2023.07.24] Feat 컴포넌트 출력과 글로벌 스타일 적용

--- src/app/qqq/qqq.component.ts
+++ src/app/qqq/qqq.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-qqq',
-  templateUrl: './qqq.component.html',
-  styleUrls: ['./qqq.component.css']
+  selector: 'qqq-component',
+  templateUrl: './qqq.component.html', // html 템플릿과 연결하기 위해 주소를 적는다.
+  // 또 이 템플릿 url 대신 <p>바로 태그로 작성 가능</p> 이렇게 바로 태그로 작성이 가능하다.
+  // 바로 작성을 하려면 template 속성을 이용해야 한다.
+  styleUrls: ['./qqq.component.css'],
 })
-export class QqqComponent {
-
-}
+export class QqqComponent {}
