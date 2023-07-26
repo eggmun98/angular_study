ab1508dcced86372c6352b7b8fd028205e3f0251
eggmun98 <eggmun98@gmail.com>
2023-07-26 10:04:51 +0900

    [2023.07.26] Comment 타입에 대한 주석 추가

--- src/app/ifif/ifif.component.ts
+++ src/app/ifif/ifif.component.ts
@@ -10,6 +10,9 @@ export class IfifComponent {
 
   onChangeData(event: Event) {
     this.eventData = (event.target as HTMLInputElement).value;
+    // target 속성은 EventTarget 인터페이스를 따른다.
+    // 하지만 EventTarget 인터페이스에서는 value 속성이 없다.
+    // 그래서 as를 통해서 HTMLInputElement 타입을 써줘야 한다.
     console.log(this.eventData);
   }
 
